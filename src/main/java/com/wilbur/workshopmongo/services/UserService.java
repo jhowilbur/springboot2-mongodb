d115c6c630da8584f2f499b35dab3fb8b52caeaa
JhoWilbur <office.wilbur@gmail.com>
2020-04-14 12:09:42 -0300

    User selection with DELETE

--- src/main/java/com/wilbur/workshopmongo/services/UserService.java
+++ src/main/java/com/wilbur/workshopmongo/services/UserService.java
@@ -34,4 +34,9 @@ public class UserService {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
 }
